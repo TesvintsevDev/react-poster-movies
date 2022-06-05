e8920f7dfaa0703f4d4896b51b99db25065feb4e
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-06-05 20:37:35 +0300

    creating a movie list

---
+++ src/components/Movies.jsx
@@ -0,0 +1,17 @@
+// Вывод карточек с фильмами
+import { Movie } from './Movie';
+
+function Movies(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className='movies'>
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
+    );
+}
+export { Movies };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,29 @@
-function Main() {
-	return <main className="container content">
-		Main from Movies App
-	</main>
+import React from 'react';
+import { Movies } from '../components/Movies';
+
+class Main extends React.Component {
+	state = {
+		movies: [],
+	}
+
+	componentDidMount() {
+		fetch('http://www.omdbapi.com/?apikey=67e22509&s=matrix')
+			.then(response => response.json())
+			.then(data => this.setState({movies: data.Search}))
+	}
+
+	render() {
+		const {movies} = this.state
+
+
+		return <main className='container content'>
+			{
+				movies.length ? (
+					<Movies movies = {this.state.movies}/>
+				) : <h3>Loading..</h3>
+			}
+		</main>
+	}
 }
 
-export {Main}
+export { Main };
