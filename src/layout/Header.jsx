5398121d66b4a358685eb3b5aa6fa28c04e8b053
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-06-08 01:01:03 +0300

    added search

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="nav-wrapper">
         <a href="#" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="#">Repo</a></li>
         </ul>
         </div>
     </nav>
